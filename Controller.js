0d97a97cfee31033830b3eb524a6cd801bad1ed0
Kieran <K-Embee@protonmail.com>
2020-08-25 00:35:51 +0200

    Changed - Fixed movement bug in Chrome/Edge where the character would randomly start moving constantly in one direction.

--- Controller.js
+++ Controller.js
@@ -1,6 +1,7 @@
 class Controller{
     constructor(){
         this.keyState = {
+            status:[false, false, false, false], // Down, Up, Left, Right. False = Up, True = Down
             direction:[0,0], //Non-normalized movement vector based on keypresses
             mousePos:[0,0], //Mouse location vector, bound between 0 and 1 (both axes, regardless of aspet ratio)
             mouseButton:false //Whether the left mouse is being held or not
@@ -18,31 +19,53 @@ class Controller{
     keyStateSet(event) {
         if(event.repeat) { return; }
         if(event.key == 'ArrowDown' || event.key.toLowerCase() == 's') {
-            this.keyState.direction[1] +=1;
+            this.keyState.status[0] = true;
         }
         else if(event.key == 'ArrowUp' || event.key.toLowerCase() == 'w') {
-            this.keyState.direction[1] -=1;
+            this.keyState.status[1] = true;
         }
         else if(event.key == 'ArrowLeft' || event.key.toLowerCase() == 'a') {
-        this.keyState.direction[0] -=1;
+        this.keyState.status[2] = true;
         }
         else if(event.key == 'ArrowRight' || event.key.toLowerCase() == 'd') {
-            this.keyState.direction[0] +=1;
+            this.keyState.status[3] = true;
         }
+
+        this.recalcDirection()
     }
 
     keyStateUnset(event) {
         if(event.key == 'ArrowDown' || event.key.toLowerCase() == 's') {
-            this.keyState.direction[1] -=1;
+            this.keyState.status[0] = false;
         }
         else if(event.key == 'ArrowUp' || event.key.toLowerCase() == 'w') {
-            this.keyState.direction[1] +=1;
+            this.keyState.status[1] = false;
         }
         else if(event.key == 'ArrowLeft' || event.key.toLowerCase() == 'a') {
-            this.keyState.direction[0] +=1;
+        this.keyState.status[2] = false;
         }
         else if(event.key == 'ArrowRight' || event.key.toLowerCase() == 'd') {
-            this.keyState.direction[0] -=1;
+            this.keyState.status[3] = false;
+        }
+
+        this.recalcDirection();
+    }
+
+    recalcDirection(){
+        this.keyState.direction[0] = 0;
+        this.keyState.direction[1] = 0;
+
+        if(this.keyState.status[0] == true) {
+            this.keyState.direction[1] +=1;
+        }
+        else if(this.keyState.status[1] == true) {
+            this.keyState.direction[1] -=1;
+        }
+        else if(this.keyState.status[2] == true) {
+        this.keyState.direction[0] -=1;
+        }
+        else if(this.keyState.status[3] == true) {
+            this.keyState.direction[0] +=1;
         }
     }
 
